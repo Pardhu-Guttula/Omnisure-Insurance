6941fce3c2b81cce39a61278e169582746c43afb
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-27 02:17:33 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,9 @@
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources"
+  description = "The name of the resource group"
   type        = string
 }
 
-variable "resource_group_location" {
-  description = "The Azure region where the resources will be created"
+variable "location" {
+  description = "The location of the resource group"
   type        = string
-}
-
-variable "sql_admin_username" {
-  description = "The administrator username for SQL Server"
-  type        = string
-}
-
-variable "sql_admin_password" {
-  description = "The administrator password for SQL Server"
-  type        = string
-  sensitive   = true
 }
