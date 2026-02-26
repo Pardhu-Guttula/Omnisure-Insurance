6f7bad02320dc2798b43a98254ef6256788ae4a4
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-27 02:13:10 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,93 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
-  version = "~> 4.56.0"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "example-resources"
-  location = "East US"
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
-resource "azurerm_api_management" "apim" {
-  name                = "example-apim"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = "Example Publisher"
-  publisher_email     = "publisher@example.com"
-  sku_name            = "Developer_1"
-}
-
-resource "azurerm_api_management_api" "example_api" {
-  name                = "example-api"
-  resource_group_name = azurerm_resource_group.rg.name
-  api_management_name = azurerm_api_management.apim.name
-  revision            = "1"
-  display_name        = "Example API"
-  path                = "example"
-  protocols           = ["https"]
-}
-
-resource "azurerm_api_management_api_operation" "get" {
-  operation_id        = "get"
-  api_name            = azurerm_api_management_api.example_api.name
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = azurerm_resource_group.rg.name
-  display_name        = "GET Example"
-  method              = "GET"
-  url_template        = "/"
-  response {
-    status       = 200
-    description  = "OK"
-  }
-}
-
-resource "azurerm_b2c_directory" "example" {
-  name                = "example-directory"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku {
-    name                = "Basic"
-    capacity           = 1
+    tier = "Basic"
+    size = "B1"
   }
+}
+
+resource "azurerm_app_service" "backend_api" {
+  name                = "example-backend-api"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "example-functionapp"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+  runtime                    = "node"
+  version                    = "~14"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "sqlserverexample"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "example-sqldb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Basic"
+  requested_service_objective_name = "Basic"
+  max_size_gb         = 5
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+}
+
+output "app_service_default_hostname" {
+  value = azurerm_app_service.backend_api.default_site_hostname
+}
+
+output "function_app_default_hostname" {
+  value = azurerm_function_app.example.default_hostname
 }
