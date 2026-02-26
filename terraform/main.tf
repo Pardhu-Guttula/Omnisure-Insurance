75e278fdd8f1c6c327b30fb1a21a601c07cf3a72
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-27 02:21:50 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,89 +1,56 @@
 provider "azurerm" {
   features {}
+  version = "~> 4.56.0"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = "appserviceplan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  publisher_name      = "examplepublisher"
+  publisher_email     = "pub@example.com"
+  sku_name            = "Developer_1"
 }
 
-resource "azurerm_app_service" "app" {
-  name                = "appservice"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+resource "azurerm_logic_app_workflow" "example" {
+  name                = "example-workflow"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
 
-resource "azurerm_cdn_profile" "example" {
-  name                = "exampleCDNProfile"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard_Verizon"
-}
-
-resource "azurerm_cdn_endpoint" "example" {
-  name                = "exampleCDNEndpoint"
-  profile_name        = azurerm_cdn_profile.example.name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  origin {
-    name      = "example"
-    host_name = azurerm_app_service.app.default_site_hostname
-  }
-}
-
-resource "azurerm_function_app" "fa" {
-  name                = "functionapp"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-  storage_account_name = azurerm_storage_account.sa.name
-  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  version            = "~3"
-}
-
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "example" {
   name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_api_management" "apim" {
-  name                = "exampleAPImanagement"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = "MyCompany"
-  publisher_email     = "company@example.com"
-  sku_name            = "Developer_1"
+resource "azurerm_app_service" "policy_management" {
+  name                = "policy-management"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
-resource "azurerm_monitor_diagnostic_setting" "ads" {
-  name                           = "example-diagnostic-setting"
-  target_resource_id             = azurerm_app_service.app.id
-  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
-  enabled_log {
-    category = "AppServiceHTTPLogs"
-  }
-  metric {
-    category = "AllMetrics"
-  }
+resource "azurerm_app_service" "claims_tracking" {
+  name                = "claims-tracking"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "example-law"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
