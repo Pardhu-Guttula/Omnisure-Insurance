c94bbfe10482891467600c71f050d35a6114d98e
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-27 02:21:51 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
+  default     = "example-resources"
 }
 
 variable "location" {
-  description = "The location of the resource group"
+  description = "The Azure region where resources will be deployed"
   type        = string
+  default     = "East US"
 }
